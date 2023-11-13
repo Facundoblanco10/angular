bd7e405fbe471ef5bc1d1ac3451155478d2bde37
Facundo <fblanco@oneplay.net>
2023-11-13 16:18:50 -0300

    Changing directory name

--- angular-fundamentals/src/app/app.component.spec.ts
+++ angular-fundamentals/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-store' title`, () => {
+  it(`should have the 'angular-fundamentals' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-store');
+    expect(app.title).toEqual('angular-fundamentals');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-store');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-fundamentals');
   });
 });
