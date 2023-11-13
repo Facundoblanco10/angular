addb2d5a65d2c79c50e2a0f88b6155c39f6b4bcf
Facundo <fblanco@oneplay.net>
2023-11-13 17:47:02 -0300

    Input and Output Communication between components

--- angular-components-and-services/src/app/app.component.ts
+++ angular-components-and-services/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ImgComponent } from './components/img/img.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ImgComponent,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-components-and-services';
+  imgParent = 'https://www.w3schools.com/howto/img_avatar.png';
+
+  onLoaded(img: string) {
+    console.log("log padre", img);
+  }
 }
---
+++ angular-components-and-services/src/app/components/img/img.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-img',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './img.component.html',
+  styleUrl: './img.component.scss'
+})
+export class ImgComponent {
+  @Input() img: string = '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = 'https://cataas.com/cat'
+
+  imgError() {
+    this.img = this.imageDefault;
+  }
+
+  imgLoaded(){
+    console.log('log hijo')
+    this.loaded.emit(this.img);
+  }
+}
