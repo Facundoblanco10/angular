38e819176b27c18702d9250c61b7f036f06b8614
Facundo <fblanco@oneplay.net>
2023-11-15 17:34:26 -0300

    Some styles

---
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css',
+})
+export class LabsComponent {
+  welcome: string = 'Welcome to Angular 17';
+  tasks: string[] = [
+    'Instalar Angular CLI',
+    'Crear proyecto',
+    'Crear componentes',
+  ];
+}
