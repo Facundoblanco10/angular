0fc1c08abf698037f2e3ad339cef4a55c6bcc8de
Facundo <fblanco@oneplay.net>
2023-11-15 10:40:58 -0300

    Services

--- angular-components-and-services/src/app/components/products/products.component.ts
+++ angular-components-and-services/src/app/components/products/products.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.model';
 import { ProductComponent } from '../product/product.component';
+import { StoreService } from '../../services/store.service';
 
 @Component({
   selector: 'app-products',
@@ -58,9 +59,12 @@ export class ProductsComponent {
     },
   ];
 
+  constructor (private storeService: StoreService) {
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
   onAddToShoppingCart(product: Product) {
-    console.log(product)
-    this.myShoppingCart.push(product);
-    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0)
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
   }
 }
