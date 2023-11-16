bb71918a10ef274824658a5decc33e6d2fb4b67b
Facundo <fblanco@oneplay.net>
2023-11-16 15:12:04 -0300

    Controlling inputs

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -1,10 +1,11 @@
 import { Component, WritableSignal, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
@@ -26,6 +27,13 @@ export class LabsComponent {
     age: 21,
     avatar: 'https://picsum.photos/200',
   });
+  colorCtrl = new FormControl();
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe((value) => {
+      console.log(value)
+    });
+  }
 
   clickHandler() {
     console.log('clickHandler');
