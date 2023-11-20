10ebca536f145a8c30755ea85c40d50d3dbd898e
Facundo <fblanco@oneplay.net>
2023-11-20 13:34:11 -0300

    Creating component of products

--- store/src/app/domains/products/pages/list/list.component.ts
+++ store/src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,36 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Producto 1',
+        price: 1000,
+        image: "https://picsum.photos/600?r=23 ",
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 2',
+        price: 120,
+        image: "https://picsum.photos/600?r=26 ",
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 3',
+        price: 400,
+        image: "https://picsum.photos/600?r=33 ",
+        creationAt: new Date().toISOString()
+      },
+    ]
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string) {
     console.log('estamos en el padre');
     console.log(event);
