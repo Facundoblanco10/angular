83140613ae29f419979e45c1ccb05d931d64cd01
Facundo <fblanco@oneplay.net>
2023-11-21 13:30:25 -0300

    Getting product data

--- store/src/app/domains/shared/services/product.service.ts
+++ store/src/app/domains/shared/services/product.service.ts
@@ -14,4 +14,7 @@ export class ProductService {
   getProducts() {
     return this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
   }
+  getOne(id: string) {
+    return this.http.get<Product>(`https://api.escuelajs.co/api/v1/products/${id}`)
+  }
 }
