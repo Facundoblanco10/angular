740734f7cd5ba4dc26f764c82a4e52e0f45e74d2
Facundo <fblanco@oneplay.net>
2023-12-01 16:39:38 -0300

    Creating an accordion

--- trello-clone/src/app/pages/boards/boards.component.ts
+++ trello-clone/src/app/pages/boards/boards.component.ts
@@ -1,20 +1,35 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faClock, faBox, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faClock,
+  faBox,
+  faWaveSquare,
+  faAngleUp,
+  faAngleDown,
+  faHeart,
+  faBorderAll,
+  faUsers,
+  faGear,
+} from '@fortawesome/free-solid-svg-icons';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [ NavbarComponent, FontAwesomeModule ],
+  imports: [NavbarComponent, FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
-
-    faClock = faClock;
-    faBox = faBox;
-    faWaveSquare = faWaveSquare;
-    faTrello = faTrello;
-
+  faClock = faClock;
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faTrello = faTrello;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faGear = faGear;
 }
