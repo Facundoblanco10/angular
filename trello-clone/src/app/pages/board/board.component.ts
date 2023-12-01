b311712a27aef153f2f2e258f3dabc0623245721
Facundo <fblanco@oneplay.net>
2023-12-01 17:45:37 -0300

    Drag & drop with sorting

---
+++ trello-clone/src/app/pages/board/board.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { ToDo } from '../../models/todo.model';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [DragDropModule, NavbarComponent],
+  templateUrl: './board.component.html',
+  styles: [
+    `
+      /* Animate items as they're being sorted. */
+      .cdk-drop-list-dragging .cdk-drag {
+        transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
+      }
+
+      /* Animate an item that has been dropped. */
+      .cdk-drag-animating {
+        transition: transform 300ms cubic-bezier(0, 0, 0.2, 1);
+      }
+    `,
+  ],
+})
+export class BoardComponent {
+  todos: ToDo[] = [
+    {
+      id: '1',
+      title: 'First todo',
+    },
+    {
+      id: '2',
+      title: 'Second todo',
+    },
+    {
+      id: '3',
+      title: 'Third todo',
+    },
+  ];
+
+  drop($event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.todos, $event.previousIndex, $event.currentIndex);
+  }
+}
