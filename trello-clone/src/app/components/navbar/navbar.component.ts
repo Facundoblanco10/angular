72503c4a5295a8f73d871791131dc9d670e36991
Facundo <fblanco@oneplay.net>
2023-12-01 10:46:55 -0300

    Icons with fontawesome

--- trello-clone/src/app/components/navbar/navbar.component.ts
+++ trello-clone/src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 import { BtnComponent } from '../btn/btn.component';
 import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell, faInfoCircle, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ BtnComponent, OverlayModule ],
+  imports: [ BtnComponent, OverlayModule, FontAwesomeModule ],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
 
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
+  faUserCircle = faUserCircle;
+
   isOpen = false;
   isOpenBody = false;
-
 
 }
