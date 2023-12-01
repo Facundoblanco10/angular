b25fa5aa478add249c77e95abb6a89b021cf51fe
Facundo <fblanco@oneplay.net>
2023-12-01 16:14:13 -0300

    Some containers

--- trello-clone/src/app/pages/boards/boards.component.ts
+++ trello-clone/src/app/pages/boards/boards.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faClock, faBox, faWaveSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [ NavbarComponent ],
+  imports: [ NavbarComponent, FontAwesomeModule ],
   templateUrl: './boards.component.html',
 })
 export class BoardsComponent {
+
+    faClock = faClock;
+    faBox = faBox;
+    faWaveSquare = faWaveSquare;
+    faTrello = faTrello;
 
 }
