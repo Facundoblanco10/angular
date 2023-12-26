609f37e545945f8188de13076c5dd404c0693a57
Facundo <fblanco@oneplay.net>
2023-12-26 12:00:48 -0300

    Styling a table

--- trello-clone/src/app/pages/table/table.component.ts
+++ trello-clone/src/app/pages/table/table.component.ts
@@ -3,11 +3,12 @@ import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { HttpClient } from '@angular/common/http';
 import { CdkTableModule } from '@angular/cdk/table';
 import { Product } from '../../models/product.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [NavbarComponent, CdkTableModule],
+  imports: [NavbarComponent, CdkTableModule, NgClass],
   templateUrl: './table.component.html',
 })
 export class TableComponent {
@@ -15,12 +16,16 @@ export class TableComponent {
 
   products: Product[] = [];
   columns: string[] = ['Number', 'Name', 'price', 'cover'];
+  totalPrice: number = 0;
 
   ngOnInit() {
     this.http
       .get<Product[]>('https://api.escuelajs.co/api/v1/products')
       .subscribe((data) => {
         this.products = data;
+        this.totalPrice = this.products
+          .map((product) => product.price)
+          .reduce((a, b) => a + b, 0);
       });
   }
 }
